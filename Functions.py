3476a6ef7b42ad6e88883388b1e6b7ac21b5f12c
Calil Gomes <75040393+Caloka@users.noreply.github.com>
2023-06-20 22:56:34 -0300

    Create Functions.py

---
+++ Functions.py
@@ -0,0 +1,46 @@
+import math
+
+# Declarando as variáveis
+var = 30 # Vamos considerar essa variável como um ãngulo em graus
+var2 = 2 # Vamos considerar essa variável como um número inteiro
+
+
+# Utilizando funções de conversão
+varRad = math.radians(var) # Grau >>> Radianos
+varGra = math.degrees(varRad) # Radianos >>> Grau
+
+
+# Utilizando funções de trigonometria
+seno = math.sin(varRad) # Acha o seno
+cos = math.cos(varRad) # Acha o cosseno
+tan = math.tan(varRad) # Acha a tangente
+arcoseno = math.asin(varRad) # Acha o arcoseno
+arcocosseno = math.acos(varRad) # Acha o arcocosseno
+arcotangente = math.atan(varRad) # Acha o arcotangente
+
+
+# Raiz quadrada
+raiz = math.sqrt(var) # Acha a raiz quadrada de var
+
+
+# Potência
+potência1 = math.pow(var,var2) # Acha a potência de var(30) elevado à var2(2)
+''' Outa forma de calcular potência, é:'''
+potência2 = var**var2 # Acha a potência de 30 elevado à 2
+
+# Constantes
+pi = math.pi # Declara o pi como variável
+neperiano = math.e # Declara o número neperiano como variável
+infinito = math.inf # Declara o valor infinito como variável
+
+
+# Logarítmo
+ln =  math.log(var) # Encontra o logarítmo natural(ln) da variável 'var' 
+log = math.log10(var) # Encontra o logarítmo da variável 'var' na base 10
+log1 = math.log(var,10) # Outra forma de encontrar o logarítmo de 'var' na base 10
+
+
+# Números complexos
+''' 2 formas de declarar números complexos no pytho, vejamos:'''
+num = 3j # Apenas adicionando um 'j' ao lado do valor da variável
+num2 = complex(2,1) # usando a classe complex já incluida no próprio python. Aqui ele retornar: 2 + 1j
